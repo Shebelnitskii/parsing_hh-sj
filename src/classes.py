c5d316cae6c0f310c4bfdcecb6455ba4ab61e886
Shebel <Shebelnitskiy@gmail.com>
2023-03-31 14:38:48 +0400

    fix class SuperJob, add sj_api_key

--- src/classes.py
+++ src/classes.py
@@ -1,4 +1,5 @@
 from abc import ABC,abstractmethod
+import os
 import requests
 class Engine(ABC):
     @abstractmethod
@@ -21,8 +22,9 @@ class HH(Engine):
         return data
 
 class SuperJob(Engine):
-    def __init__(self, token: str):
-        self.headers = {'X-Api-App-Id': token}
+    def __init__(self):
+        sj_api_key: str = os.getenv('SJ_API_KEY')
+        self.headers = {'X-Api-App-Id': sj_api_key}
 
     def get_request(self, url: str, params: dict) -> dict:
         response = requests.get(url, headers=self.headers, params=params)
@@ -32,7 +34,12 @@ class SuperJob(Engine):
             data = {}
         return data
 
-hh = HH()
-url = "https://api.hh.ru/vacancies"
-params = {'text': 'python', 'per_page': 10}
-print(hh.get_request(url,params))
+# hh = HH()
+# url = "https://api.hh.ru/vacancies"
+# params = {'text': 'python', 'per_page': 10}
+# print(hh.get_request(url,params))
+
+sj= SuperJob()
+url_sj = "https://api.superjob.ru/2.0/vacancies/"
+params_sj = {'keyword': 'python', 'per_page': 10}
+print(sj.get_request(url_sj,params_sj))
