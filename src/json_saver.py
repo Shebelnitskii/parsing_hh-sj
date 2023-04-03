c996c106fe2fe4b43fabff295133dc2990076871
Shebel <Shebelnitskiy@gmail.com>
2023-04-03 16:44:44 +0400

    fix method add_vacancy by class JSONSaver

--- src/json_saver.py
+++ src/json_saver.py
@@ -1,27 +1,54 @@
 import json
 import re
+import os
+
+
 class JSONSaver:
     def __init__(self):
-        self.file_name: str = 'JSON_HH'
-        self.vacancies = []
-    def add_vacancy(self, vacancies):
+        self.file_name: str = 'JSON.json'
+
+    def add_vacancy(self, vacanciesHH=None, vacanciesSJ=None):
         """
         Сохраняет информацию о вакансиях в файл JSON.
-
-        :param vacancies: Список экземпляров класса VacancyHH.
-        :param file_name: Имя файла для сохранения.
         """
-        self.vacancies = vacancies
-        with open(self.file_name, 'w', encoding='utf-8') as file:
-            json.dump([vars(vacancy) for vacancy in vacancies], file, ensure_ascii=False, indent=4)
+        if vacanciesHH is None and vacanciesSJ is None:
+            print("Введите хотя бы одно значение")
+        elif vacanciesHH is not None and vacanciesSJ is None:
+            with open('JSON_HH.json', 'w', encoding='utf-8') as file:
+                json.dump(
+                    sorted([vars(vacancy) for vacancy in vacanciesHH], key=lambda x: x['salary']['from'], reverse=True),
+                    file, ensure_ascii=False, indent=4)
+        elif vacanciesSJ is not None and vacanciesHH is None:
+            with open('JSON_SJ.json', 'w', encoding='utf-8') as file:
+                json.dump(
+                    sorted([vars(vacancy) for vacancy in vacanciesSJ], key=lambda x: x['salary']['from'], reverse=True),
+                    file, ensure_ascii=False, indent=4)
+        else:
+            with open('JSON_HH.json', 'w', encoding='utf-8') as file:
+                json.dump([vars(vacancy) for vacancy in vacanciesHH], file, ensure_ascii=False, indent=4)
+            with open('JSON_SJ.json', 'w', encoding='utf-8') as file:
+                json.dump([vars(vacancy) for vacancy in vacanciesSJ], file, ensure_ascii=False, indent=4)
+            # Открыть первый файл JSON и прочитать данные
+            with open('JSON_HH.json', 'r', encoding='utf-8') as f:
+                json_hh = json.load(f)
 
+            # Открыть второй файл JSON и прочитать данные
+            with open('JSON_SJ.json', 'r', encoding='utf-8') as f:
+                json_sj = json.load(f)
+            vacancies = json_hh + json_sj
+            with open(self.file_name, 'w', encoding='utf-8') as f:
+                json.dump(vacancies, f, ensure_ascii=False, indent=4)
+
+            # удаляем JSON-файлы
+            os.remove("JSON_HH.json")
+            os.remove("JSON_SJ.json")
 
     def clear_json(self):
         """
         Очищает файл JSON.
         """
         with open(self.file_name, 'w', encoding='utf-8') as file:
-            json.dump('', file, ensure_ascii=False, indent=4)
+            json.dump([], file)
 
     def get_vacancies_by_salary(self, salary_input):
         """
@@ -37,14 +64,10 @@ class JSONSaver:
             salary, currency = salary_input.split(' ')
             for vac in vacancy:
                 try:
-                    if '-' in vac['salary']:
-                        salary_split = vac['salary'].split('-')
-                        if int(salary) <= int(salary_split[0]):
-                            test_dict.append(vac)
-                    else:
-                        salary_split = vac['salary'].split()
-                        if int(salary) <= int(salary_split[0]):
-                            test_dict.append(vac)
+                    if int(vac['salary']['from']) >= int(salary) and str(vac['salary']['currency']) in ['RUR', 'rub']:
+                        test_dict.append(vac)
+                    elif vac['salary']['currency'].lower() in ['usd', 'USD']:
+                        test_dict.append(vac)
                 except:
                     continue
             json.dump(test_dict, file, ensure_ascii=False, indent=4)
