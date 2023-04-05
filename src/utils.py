9b078a599e2178b55ee625077fa4c0f15fb5e503
Shebel <Shebelnitskiy@gmail.com>
2023-04-05 17:53:59 +0400

    add function experience_input, fix bugs

--- src/utils.py
+++ src/utils.py
@@ -5,21 +5,10 @@ from src.json_saver import JSONSaver
 def user_interaction():
     try:
         hh_api, sj_api = check_platform()
-        search_query = str(input("Введите поисковый запрос: "))
-        try:
-            hh_vacancies, sj_vacancies = get_class_by_platform(hh_api, sj_api, search_query)
-        except:
-            hh_vacancies, sj_vacancies = None, None
-        salary_sort = input("Введите минимальную зарплату для поиска(rub): ")
-        try:
-            if salary_sort.strip() == '':
-                salary_sort = 0
-                print("Вы не ввели минимальную зарплату")
-        except ValueError:
-            salary_sort = 0
-            print("Некорректное значение. Минимальное значение будет равно 0")
-        filter_words = input("Введите ключевые слова для фильтрации вакансий в описании: ")
-        output_results(hh_vacancies, sj_vacancies, salary_sort, filter_words)
+        hh_vacancies, sj_vacancies = get_class_by_platform(hh_api, sj_api)
+        filter_word_input = filter_words()
+        salary_input = salary_sort()
+        output_results(hh_vacancies, sj_vacancies, filter_word_input, salary_input)
     except:
         print('До скорой встречи!')
 
@@ -30,7 +19,7 @@ def check_platform():
         platform_input = input('Введите платформы для поиска (HeadHunter\SuperJob): ').lower().split(' ')
         if 'выход' in platform_input or 'exit' in platform_input:
             print('Выход из программы')
-            return False
+            raise SystemExit
         elif set(platform_input) & {'headhunter', 'hh'} and set(platform_input) & {'superjob', 'sj'}:
             hh_api = src.classes.HeadHunterAPI()
             sj_api = src.classes.SuperJobAPI()
@@ -51,25 +40,82 @@ def check_platform():
             print('Вы не ввели платформу')
             continue
 
-def get_class_by_platform(hh_api, sj_api, search_word):
+
+def get_class_by_platform(hh_api, sj_api):
+    search_query = str(input("Введите поисковый запрос: "))
+    hh_exp, sj_exp = experience_input()
+    if 'выход' in search_query or 'exit' in search_query:
+        print('Выход из программы')
+        raise SystemExit
     if hh_api is not None and sj_api is not None:
-        hh_vacancies = hh_api.get_class_vacancies(search_word)
-        sj_vacancies = sj_api.get_class_vacancies(search_word)
+        hh_vacancies = hh_api.get_class_vacancies(hh_exp, search_query)
+        sj_vacancies = sj_api.get_class_vacancies(sj_exp, search_query)
         return hh_vacancies, sj_vacancies
     if sj_api is not None:
-        sj_vacancies = sj_api.get_class_vacancies(search_word)
+        sj_vacancies = sj_api.get_class_vacancies(sj_exp, search_query)
         return None, sj_vacancies
     if hh_api is not None:
-        hh_vacancies = hh_api.get_class_vacancies(search_word)
+        hh_vacancies = hh_api.get_class_vacancies(hh_exp, search_query)
         return hh_vacancies, None
 
-def output_results(hh_vacancies, sj_vacancies, salary_sort, filter_words):
+
+def salary_sort():
+    while True:
+        salary_min = input("Введите минимальную зарплату для поиска (rub): ")
+        if 'выход' in salary_min or 'exit' in salary_min:
+            print('Выход из программы')
+            raise SystemExit
+        if not salary_min.strip():
+            print("Вы не ввели минимальную зарплату. Минимальное значение будет равно 0")
+            return '0'
+        try:
+            salary_min = int(salary_min)
+            return salary_min
+        except ValueError:
+            print("Некорректное значение. Минимальное значение будет равно 0")
+            return '0'
+
+
+def filter_words():
+    words = input(
+        "Введите ключевые слова для фильтрации вакансий в описании(чем больше слов тем больше найдётся соответствий):\n")
+    if 'выход' in words or 'exit' in words:
+        print('Выход из программы')
+        raise SystemExit
+    elif words == '':
+        print('Вы не ввели никаких слов')
+        return words
+    else:
+        return words
+
+
+def experience_input():
+    hh_experience = ["noExperience", "between1And3", "between3And6", "moreThan6"]
+    sj_experience = {"1": "без опыта", "2": "от 1 года", "3": "от 3 лет", "4": "от 6 лет"}
+    print("Выберите опыт работы:\n1. Без опыта\n2. От 1 года до 3 лет\n3. От 3 до 6 лет\n4. Более 6 лет")
+    choice = input("Введите номер: ")
+    if 'выход' in choice or 'exit' in choice:
+        print('Выход из программы')
+        raise SystemExit
+
+    if choice not in ["1", "2", "3", "4"]:
+        print("Неверный ввод. По умолчанию будет выбран вариант 'без опыта'")
+        hh_value = hh_experience[0]
+        sj_value = {'1': sj_experience['1']}
+        return hh_value, sj_value
+
+    hh_value = hh_experience[int(choice)-1]
+    sj_value = {choice: sj_experience[choice]}
+    return hh_value, sj_value
+
+def output_results(hh_vacancies, sj_vacancies, filter_words, salary_input):
     json_saver = JSONSaver()
     json_saver.add_vacancies(headhunter=hh_vacancies, superjob=sj_vacancies)
-    json_saver.get_vacancies_by_salary(salary_sort)
     json_saver.search_words(filter_words)
+    json_saver.get_vacancies_by_salary(salary_input)
     final = json_saver.json_results()
     top_n = int(input("Введите количество вакансий для вывода в топ N: "))
+    print('\n')
     if len(final) == 0:
         print('Вакансий по вашему запросу нет')
     else:
@@ -77,14 +123,13 @@ def output_results(hh_vacancies, sj_vacancies, salary_sort, filter_words):
             try:
                 text = final[x]['description'].replace('<highlighttext>', '').replace('</highlighttext>', '')
                 try:
-                    salary_text = f"{final[x]['salary']['from']}-{final[x]['salary']['to']} руб"
+                    salary_text = f"Зарплата: {final[x]['salary']['from']}-{final[x]['salary']['to']} руб"
                 except:
-                    salary_text = f"{final[x]['salary']['from']} руб"
-                print(f"{final[x]['title']}\nЗарплата: {salary_text}\nОписание вакансии:\n{text}\nСсылка: {final[x]['url']}\n")
+                    if final[x]['salary']['from'] == 0:
+                        salary_text = 'Зарплата не указана'
+                    else:
+                        salary_text = f"Зарплата: {final[x]['salary']['from']} руб"
+                print(f"{final[x]['title']}\n{salary_text}\nОписание вакансии:\n{text}\nСсылка: {final[x]['url']}\n")
             except:
                 print('Больше вакансий нет')
                 break
-
-
-
-user_interaction()
